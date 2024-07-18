cbb01fc6ae15223feaa8f00c236209b83b6e18a6
macaumang <macaumang@gmail.com>
2024-07-18 10:47:45 +0900

    first commit

--- map.v1/website/app.py
+++ map.v1/website/app.py
@@ -1,16 +1,18 @@
 # Pip install waitress
-
-from flask import Flask, render_template, request, make_response
-#from waitress import serve
-# from flask_mysqldb import MySQL
-from pandas_datareader import data 
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import pandas as pd
+import numpy as np
 from bokeh.plotting import figure, show, output_file
 from bokeh.embed import components
 from bokeh.resources import CDN
-# 
+import os  # If you need os operations
 
 
 app = Flask(__name__)
+
+
+
 app.url_map.strict_slashes = False
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
@@ -26,15 +28,23 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 def home():
     title = 'Home'
-    p = figure(title="Simple line example", x_axis_label='x', y_axis_label='y', width=450, height=450)
+    if request.method == 'POST' and 'datafile' in request.files:
+        file = request.files['datafile']
+        if file.filename != '':
+            filename = secure_filename(file.filename)
+            df = pd.read_csv(file) #Assuming the file is a CSV
 
-    p.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], legend_label="Temp.", line_width=2)
-    
-    # Generate JS and DIV components
-    script, div = components(p)
+            hist, edges = np.histogram(df, bins = 50) #adjust based on ur data's column
+            p = figure(title="Data Histogram", background_fill_color= "#fafafa" , width=400, height=400,)
+            p.quad(top=hist, bottom = 0, left = edges[:-1], right = edges[1:], fill_color = "navy", line_color = "white")
 
-    # Render template with components
-    return render_template('index.html', script=script, div=div, cdn_js=CDN.js_files[0])
+            #generate the bokeh components to embed in the HTML
+            script, div = components(p)
+            cdn_js = CDN.js_files[0]# get the CDN JS files for Bokeh
+            return render_template('index.html', script = script, div=div, cdn_js = cdn_js )
+
+    # defualt Render template with components
+    return render_template('index.html')
 
 
 
@@ -52,12 +62,18 @@ def contact():
         # return "{name}"
     return render_template('contact.html')
 
-import dataclasses
-from pandas_datareader import data as pdr
-import datetime
-from bokeh.plotting import figure, show, output_file
-from bokeh.embed import components
-from bokeh.resources import CDN
+
+from flask import Flask, request, jsonify
+from astropy.io.votable import parse_single_table
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['datafile']
+    table = parse_single_table(file.stream)
+    data = table.array
+    result = data.tolist()  # Assuming the data can be directly converted to a list
+    return jsonify(result)
+
 
 
 
--- map.v1/website/static/main.js
+++ map.v1/website/static/main.js
@@ -1,46 +1,53 @@
 
-  /*function locationHashChanged( e ) {
-    $.ajax({
-        type:'GET',
-        url:e.newURL,
-        success: function () {
-            if (window.location.hash===""){
-                $('html').load('/');
-                document.title='Home';
-            }
-            else if (window.location.href.indexOf("#") >-1){
-                $('#content').load(location.hash.replace('#','/'));
-                document.title=location.hash.replace("#","")
-            }
-            else {
-                $('html').load('/');
-                document.title='Home';
-            }
-        }
-    });
-}*/
-
-// window.onhashchange = newroutetest;
-
-// $( document ).ready(function(){
-//     newroutetest()
-// })
-
-
-// function newroutetest(){
-//     let url
-//     if (location.hash!='') {
-//         url = window.location.hash.replace('#', '/')
-//     }
-//     else{
-//         url='/home'
-//     }
-//     var xhttp = new XMLHttpRequest()
-//     xhttp.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             document.getElementById("content").innerHTML = this.responseText;
+// var aladin;
+// A.init.then(() => {
+//     aladin = A.aladin('#aladin-lite-div', {target: '05 37 58 +08 17 35', fov: 12});
+// ​
+//     var cat = A.catalog({sourceSize: 20});
+//     aladin.addCatalog(cat);
+//     cat.addSources([A.source(83.784490, 09.934156, {name: 'Meissa'}), A.source(88.792939, 7.407064, {name: 'Betelgeuse'}), A.source(81.282764, 6.349703, {name: 'Bellatrix'})]);
+// ​
+//     // define function triggered when  a source is hovered
+//     aladin.on('objectHovered', function(object) {
+//         var msg;
+//         if (object) {
+//             msg = 'You hovered object ' + object.data.name + ' located at ' + object.ra + ', ' + object.dec;
 //         }
-//     }
-//     xhttp.open("GET", url, true);
-//     xhttp.send();
-// }
+//         else {
+//             msg = 'No object hovered';
+//         }
+//         $('#infoDiv').html(msg);
+//     });
+// ​
+//     // define function triggered when an object is clicked
+//     var objClicked;
+//     aladin.on('objectClicked', function(object) {
+//         var msg;
+//         if (object) {
+//             objClicked = object;
+//           object.select();
+//             msg = 'You clicked object ' + object.data.name + ' located at ' + object.ra + ', ' + object.dec;
+//         }
+//         else {
+//             objClicked.deselect();
+//             msg = 'You clicked in void';
+//         }
+//         $('#infoDiv').html(msg);
+//     });
+// });
+
+
+// var a;
+// A.init.then(() => {
+//     a = A.aladin('#aladin-lite-div', {target: '03 47 00.00 +24 07 00.0', survey: 'P/DSS2/color', zoom: 2, showReticle: false});
+//     var cat = A.catalog({name: 'Some markers', sourceSize: 18});
+//     a.addCatalog(cat);
+//     cat.addSources([A.marker(56.87115, 24.10514, {popupTitle: 'Alcyone', popupDesc: '<em>Bmag:</em> 2.806<br/><em>Spectral type:</em> B7III<br/>More info <a target="_blank" href="https://simbad.u-strasbg.fr/simbad/sim-id?Ident=NAME%20ALCYONE&submit=submit">in Simbad</a>'})]);
+//     cat.addSources([A.marker(57.29673, 24.13671, {popupTitle: 'Pleione', popupDesc: '<em>Bmag:</em> 4.97<br/><em>Spectral type:</em> B8vne<br/>More info <a target="_blank" href="https://simbad.u-strasbg.fr/simbad/sim-id?Ident=NAME+PLEIONE&NbIdent=1">in Simbad</a>'})]);
+//     cat.addSources([A.marker(56.58156, 23.94836, {popupTitle: 'Merope', popupDesc: '<em>Bmag:</em> 4.113<br/><em>Spectral type:</em> BVI4e<br/>More info <a target="_blank" href="https://simbad.u-strasbg.fr/simbad/sim-id?Ident=V*+V971+Tau&NbIdent=1">in Simbad</a>'})]);
+//     cat.addSources([A.marker(56.45669, 24.36775, {popupTitle: 'Maia', popupDesc: '<em>Bmag:</em> 3.812<br/><em>Spectral type:</em> B8III<br/>More info <a target="_blank" href="https://simbad.u-strasbg.fr/simbad/sim-id?Ident=NAME+MAIA&NbIdent=1">in Simbad</a>'})]);
+//     cat.addSources([A.marker(56.21890, 24.11334, {popupTitle: 'Electra', popupDesc: '<em>Bmag:</em> 3.612<br/><em>Spectral type:</em> B6IIIe<br/>More info <a target="_blank" href="https://simbad.u-strasbg.fr/simbad/sim-id?Ident=NAME+ELECTRA&NbIdent=1">in Simbad</a>'})]);
+//     cat.addSources([A.marker(57.29059, 24.05342, {popupTitle: 'Atlas', popupDesc: '<em>Bmag:</em> 3.54<br/><em>Spectral type:</em> B8III<br/>More info <a target="_blank" href="https://simbad.u-strasbg.fr/simbad/sim-id?Ident=NAME+ATLAS&NbIdent=1">in Simbad</a>'})]);
+//     cat.addSources([A.marker(56.30207, 24.46728, {popupTitle: 'Taygeta', popupDesc: '<em>Bmag:</em> 4.199<br/><em>Spectral type:</em> B6IV<br/>More info <a target="_blank" href="https://simbad.u-strasbg.fr/simbad/sim-id?Ident=NAME+TAYGETA&NbIdent=1">in Simbad</a>'})]);
+// });
+
